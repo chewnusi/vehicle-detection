9b52fcf6f17dde259f85ff44c1e94579653aa867
chewnusi <katanairina2004@gmail.com>
2025-06-11 19:46:25 +0300

    Add tracker configuration and update confidence slider in app.py

--- app.py
+++ app.py
@@ -20,9 +20,17 @@ st.title("Military Equipment Detection 🔍")
 # Бічна панель
 st.sidebar.header("⚙️ Налаштування моделі")
 
-confidence = st.sidebar.slider("Виберіть рівень впевненості", 0.1, 1.0, 0.4)
+# Confidence slider
+confidence = st.sidebar.slider("Виберіть рівень впевненості", 0.05, 1.0, 0.4)
 
-# Завантаження моделі
+st.sidebar.subheader("🎯 Налаштування трекера")
+
+selected_tracker = st.sidebar.selectbox(
+    "Виберіть трекер",
+    list(config.TRACKER_TYPES.keys()),
+    index=list(config.TRACKER_TYPES.keys()).index(config.TRACKER_CONFIG['tracker_type'])
+)
+
 model_path = Path(config.DETECTION_MODEL)
 try:
     model = worker.load_model(model_path)
@@ -30,19 +38,19 @@ except Exception as ex:
     st.error(f"❌ Неможливо завантажити модель: {model_path}")
     st.error(ex)
 
-st.sidebar.header("📹 Налаштування відео")
+st.sidebar.header("Тип джерела")
 source_radio = st.sidebar.radio("Виберіть джерело", config.SOURCES_LIST)
 
 if source_radio == config.IMAGE:
     worker.detect_on_image(confidence, model)
 
 elif source_radio == config.VIDEO:
-    worker.play_stored_video(confidence, model)
+    worker.play_stored_video(confidence, model, selected_tracker)
 
 elif source_radio == config.RTSP:
-    worker.play_rtsp_stream(confidence, model)
+    worker.play_rtsp_stream(confidence, model, selected_tracker)
 
 elif source_radio == config.YOUTUBE:
-    worker.play_youtube_video(confidence, model)
+    worker.play_youtube_video(confidence, model, selected_tracker)
 else:
     st.error("❗ Будь ласка, оберіть коректний тип джерела!")
